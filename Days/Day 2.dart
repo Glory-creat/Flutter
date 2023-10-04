44e42807bc4fb16c93bc823e151f56b62b15d721
Glory Ejere <glorychristopher620@gmail.com>
2023-10-04 11:41:43 -1200

    Day 2

--- Days/Day 2.dart
+++ Days/Day 2.dart
@@ -1,6 +1,6 @@
 void main() {
   // Day 2 dart learning: Continuation of variables and data types in dart.
-  // constant: Keywords that are never gping to change.
+  // constant: Keywords that are never going to change.
   // const myConstVar = '40';
   // print('$myConstVar');
 }
