7c1ddc1cdbf94caeaacd13211df810e35d5ce5a2
Glory Ejere <glorychristopher620@gmail.com>
2023-10-04 15:58:14 -1200

    Day 2

--- Days/Day 2.dart
+++ Days/Day 2.dart
@@ -1,6 +1,22 @@
 void main() {
   // Day 2 dart learning: Continuation of variables and data types in dart.
-  // constant: Keywords that are never going to change.
-  // const myConstVar = '40';
-  // print('$myConstVar');
+  // Constant variables and Final variables
+
+  // Final variable (The final variable is initialised only once)
+  // Here is an example of how to create a final variable:
+  // final cityName = 'Lagos';
+  final String cityName = 'Lagos';  // with type 'string' annotation
+  // cityName 'Ogun'; // You cannot reassign final variable
+
+  // Constant variable
+  // Here is an example of how to create a constant variable:
+  // const pi = '4.21';
+  const double pi = 5.552;  // with type 'string' annotation
+  const bool isDartFun = true;
+  // pi 5.552; // You cannot reassign a constant variable
+  
+  // Printing variables
+  print('$cityName');
+  print('$pi');
+  print('Is Dart fun? $isDartFun');
 }
