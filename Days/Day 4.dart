4a7783896792560e5a504485016241b9ec2eae12
Glory Ejere <glorychristopher620@gmail.com>
2023-10-06 00:00:49 -1200

    Day 4

--- Days/Day 4.dart
+++ Days/Day 4.dart
@@ -1,23 +1,23 @@
-void main() {
+// void main() {
   
-  const String Text = 'Hi';
-  print('$Text');
+//   const String Text = 'Hi';
+//   print('$Text');
 
-  int x = 2;
-  int result = x*6;
-  print ("Print $result");
-  String result2 = 'yo';
-  // var identifier = 'the';
+//   int x = 2;
+//   int result = x*6;
+//   print ("Print $result");
+//   String result2 = 'yo';
+//   // var identifier = 'the';
   
-  print("The length of this string value is ${result2.length}");
+//   print("The length of this string value is ${result2.length}");
 
-  var object = '(\'it\'s correct\')'; //The purpose of the backslash to escape single quotes
-  // var object2 = "it's correct"; //Using double quote does not affect escaping
-  print('$object is correct'); //where "\" serves as an escape character
+//   var object = '(\'it\'s correct\')'; //The purpose of the backslash to escape single quotes
+//   // var object2 = "it's correct"; //Using double quote does not affect escaping
+//   print('$object is correct'); //where "\" serves as an escape character
   
-  String $object3 = '''This is going to be a very long sentence. 
-  A very very long sentence, This sentence is kinda long
-  This statement is short too but I can't use lorem ipsum''';
+//   String $object3 = '''This is going to be a very long sentence. 
+//   A very very long sentence, This sentence is kinda long
+//   This statement is short too but I can't use lorem ipsum''';
 
-  print('${$object3}'); //Variable name can only "$", "letters", "numbers" and "_"
-}
+//   print('${$object3}'); //Variable name can only "$", "letters", "numbers" and "_"
+// }
