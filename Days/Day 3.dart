9552b2bb39edb0139550362562d87624ecc56f4c
Glory Ejere <glorychristopher620@gmail.com>
2023-10-05 23:59:17 -1200

    Day 4

--- Days/Day 3.dart
+++ Days/Day 3.dart
@@ -1,8 +1,3 @@
-void main() {
-  const String greeting = 'Hi';
-  print('$greeting');
-
-  int x = 2;
-  y = 4;
-  print('$(x*y)');
-}
+// void main() {
+//   String? name;
+// }
---
+++ Days/Day 4.dart
@@ -0,0 +1,23 @@
+void main() {
+  
+  const String Text = 'Hi';
+  print('$Text');
+
+  int x = 2;
+  int result = x*6;
+  print ("Print $result");
+  String result2 = 'yo';
+  // var identifier = 'the';
+  
+  print("The length of this string value is ${result2.length}");
+
+  var object = '(\'it\'s correct\')'; //The purpose of the backslash to escape single quotes
+  // var object2 = "it's correct"; //Using double quote does not affect escaping
+  print('$object is correct'); //where "\" serves as an escape character
+  
+  String $object3 = '''This is going to be a very long sentence. 
+  A very very long sentence, This sentence is kinda long
+  This statement is short too but I can't use lorem ipsum''';
+
+  print('${$object3}'); //Variable name can only "$", "letters", "numbers" and "_"
+}
