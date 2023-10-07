8477089ed72131b0b1d30985b18e4bd01b2634f9
Glory Ejere <glorychristopher620@gmail.com>
2023-10-06 21:58:09 -1200

    Edit

--- Days/Day 5.dart
+++ Days/Day 5.dart
@@ -1,4 +1,16 @@
 void main() {
     // Collections //In dart collections are used to store and manage multiple values.
-    //Collection type in dart
+    //Collection type in dart. The collection types in dart are:List, Sets and Maps
+
+    // List //List is an ordered collection of elements.
+    //Lists can be created by using 'List' class or a list literal.
+    List<String> names = ['Glory, Ibukun, Ebun, Ebun'];
+    print('$names');
+
+    // Set //Set is an unordered collection of elements.
+    // Set can be created using the 'Set' class or literal. //Sets do not allow to duplicate value.
+    Set<String> numbers = {'one, two, three, three'};
+    print('$numbers');
+
+
 }
