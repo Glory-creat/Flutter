79f0abc9de967dc10953098708e558dbd8c15a98
Glory Ejere <glorychristopher620@gmail.com>
2023-10-07 09:47:20 -1200

    Edit

--- Days/Day 5.dart
+++ Days/Day 5.dart
@@ -5,19 +5,26 @@ void main() {
     // List //List is an ordered collection of elements.
     //Lists can be created by using 'List' class or a list literal.
     List<String> names = ['Glory, Ibukun, Ebun, Ebun'];
+    List<int> _numbers = [1,2,3,3];
+
+    // Print
     print('$names');
+    print ('$_numbers');
 
     // var Names = ['Glory, Ibukun, Ebun, Ebun'];
     // print('$Names');
 
-    // Set //Set is an unordered collection of elements.
+    // Set 
+    ////Set is an unordered collection of elements.
     // Set can be created using the 'Set' class or literal. //Sets do not allow to duplicate value.
     Set<String> numbers = {'one, two, three, three'};
     print('$numbers');
 
-    // Map //Key/Value pairs?
+    // Map 
+    //Key/Value pairs.
+    // Each key in a map is unique, and you can use the key to retrieve its associated value.
     Map<String, int> unrelated = {
-      "Score":30,
+      "Score":30, // where 'Score' is the key and '30' is the associated value
       "Age": 32 
       };
 
