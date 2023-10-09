c9ecea6347532e43151983b95dc0108a75c0e5e9
Glory Ejere <glorychristopher620@gmail.com>
2023-10-08 15:26:32 -1200

    Day 6

--- Days/Day 6.dart
+++ Days/Day 6.dart
@@ -1,22 +1,26 @@
 void main() {
   // Exploring collections in dart
+
+  //List
+  //In dart, array is known as list.
+
   // List (Fixed-length list)
   
   // Element: 90 20 16 5  2  4
   // Index:   0  1  2  3  4  5 
   List<int> numberlist = [6];
-  // numberlist [0] = 90;
-  // numberlist [1] = 20;
-  // numberlist [2] = 16;
-  // numberlist [3] = 5;
-  // numberlist [4] = 2;
-  // numberlist [6] = 4;
+   numberlist [0] = 90;
+   numberlist [1] = 20;
+   numberlist [2] = 16;
+   numberlist [3] = 5;
+   numberlist [4] = 2;
+   numberlist [5] = 4;
 
-  print(numberlist);
+  print(numberlist[0]);
 
-  if(numberlist[0]== 6) {
-    print('Correct!');
-  } else{
-    print('Not correct!');
-  }
+  // if(numberlist[0]== 6) {
+  //   print('Correct!');
+  // } else{
+  //   print('Not correct!');
+  // }
 }
