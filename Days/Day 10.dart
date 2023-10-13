7e1c5ffff10956924accd84249a04448c791080d
Glory Ejere <glorychristopher620@gmail.com>
2023-10-12 15:42:23 -1200

    Day 10

---
+++ Days/Day 10.dart
@@ -0,0 +1,64 @@
+void main() {
+  //Operators in dart
+
+  // Dart, like many programming languages, supports a variety 
+  // of operators that can be used to perform operations on variables and values.
+
+  //Here is a list of some common operators
+  //1. Arithmetic operators
+  //2. Equality and Rational operators
+  //3. Type test operators
+  //4. Bitwise operators
+  //5. Logical operators
+
+  //1. Arithmetic operators
+
+  //Examples of Arithmetic operators are:
+
+  // '+' (Addition)
+
+   int x = 30;
+   double y = 44.4;
+  
+   print(x + y); //Adding up the value of the 'int' and 'double,
+
+  
+  // '-' (Subtraction)
+
+   int n = 50;
+   double o = 44.4;
+
+   print(n - o); //Subtracting the value of the 'int' and 'double,
+
+
+  // '/' (Division)
+    
+    int g = 50;
+    double s = 44.4;
+  
+    print(g / s + 44); //Dividing the value of the 'int' by 'double' + 44,
+
+
+  // '*' (Multiplication)
+
+    int k = 30;
+    double q = 44.4;
+  
+    print(k * q);  //Multiplying the value of the 'int' and 'double,
+
+
+  // '%' (Modulo, returns the remainder after division)
+
+    int u = 30;
+    double z = 2;
+
+    print(u % z);  //Print to get remainder after division
+
+
+  // '~/' (Integer division, return the integer part of the division result)
+    int t = 30;
+    double l = 2;
+
+    print(t ~/ l);  //Print to get integer division
+
+}
