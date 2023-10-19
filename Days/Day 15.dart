a3dbb7d0ff55f491a16ff77cd46c2adf143ea268
Glory Ejere <glorychristopher620@gmail.com>
2023-10-18 15:04:09 -1200

    Edit

--- Days/Day 15.dart
+++ Days/Day 15.dart
@@ -1,3 +1,37 @@
 void main() {
+  // Practice 
+
+  // Question 1
+
+  // Variables:
+
+  // Declare a variable for each of the following data types: 
+  //int, double, String, and bool. Assign values to them and print their values.
+  //Create a program that calculates the area of a rectangle. 
+  //You should take the length and width as input from the user and store them in variables.
+  
+  // 1. int
+  final number = <int>[40,50];
+  const myConstNumber = <int>[60,70];
+  
+  // Printing values
+  print(myConstNumber);
+  print(number);
+  print("Length of 'number is ${number.length}");
+
+
+  // 2. double 
+  const decimal = <double> [45];
+  final decimal1 = <double> [80];
+
+  
+
+  // 3. String
+  // String _character = 'This is day 15 is\'nt it?';
+
+  // 4. bool
+  bool isTrue = true;
+  print(isTrue);
+
   
 }
