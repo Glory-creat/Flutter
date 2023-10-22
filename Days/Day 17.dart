d6f72cbe198adadfaffab586d50524940bfcc59b
Glory Ejere <glorychristopher620@gmail.com>
2023-10-21 19:53:25 -1200

    Day 17

--- Days/Day 17.dart
+++ Days/Day 17.dart
@@ -28,4 +28,34 @@ void main() {
   } else {
     print('Contact IT for help');
   }
+  //  Task: Create a Dart program that takes a user's age as input and determines whether they are eligible to vote in an election or not. The legal voting age is 18 or older. Your program should perform the following steps:
+  // Prompt the user to enter their age.
+   // Read and store the user's age as an integer.
+   // Use an IF-ELSE statement to check if the user's age is 18 or older.
+   // If the age is 18 or older, print "You are eligible to vote."
+   // If the age is below 18, print "You are not eligible to vote."
+
+   int age = 18;
+
+   if(age > 18) {
+    print('You are eligible to vote');
+   } else if(age < 18) {
+    print('You are not eligible to vote');
+   } else {
+    print('You are 18 and you are eligible to vote');
+   }
+   
+   
+  //  Create a Dart program that determines whether a
+  // given integer is even or odd and displays a message accordingly.
+
+  int option = 3;
+
+  if(option == 3) {
+    print('$option is an odd number');
+  } else if(option == 2) {
+    print('It is an even number');
+  } else {
+    print('$option is an odd number');
+  }
 }
