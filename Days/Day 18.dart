a365d4be8c014f55d4a7f87e2e5fdf49481d355b
Glory Ejere <glorychristopher620@gmail.com>
2023-10-22 16:19:31 -1200

    Day 18

---
+++ Days/Day 18.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Conditional expressions
+
+  // Dart supports two main types of conditional expressions: the 'if-else' conditional expression and the 
+  // 'null-aware' conditional expression.
+
+  // 'if-else' conditional expression
+}
