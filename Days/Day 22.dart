b19483d68066f4f1d5bd077b9f08350d1fad63e1
Glory Ejere <glorychristopher620@gmail.com>
2023-10-27 23:10:47 -1200

    Day 22.dart

--- Days/Day 22.dart
+++ Days/Day 22.dart
@@ -1,3 +1,19 @@
+  // Giving input to computer makes a code more dynamic
+  // importing the package import 'dart:io'; is useful for user input
+  // you can take standard input from the user by using the .readLineSync() function
+import 'dart:io';
 void main() {
-  
+  // String user input
+  // print('Expression');
+  // String? trial = stdin.readLineSync();
+  // print('$trial');
+
+  print('Enter name:');
+  String? name = stdin.readLineSync();
+  print('$name Adaolisa Ejere');
+
+  // Integer user input
+  print('integer input');
+  int? number = int.parse(stdin.readLineSync()!);
+  print('$number');
 }
