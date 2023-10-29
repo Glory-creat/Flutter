bf5ca0bb782a57183d7c99bd754ba94086c3660a
Glory Ejere <glorychristopher620@gmail.com>
2023-10-28 22:32:03 -1200

    Day 23

---
+++ Days/Day 23.dart
@@ -0,0 +1,10 @@
+void main() {
+  String day = 'day twenty-three';
+  print('${day[0].toUpperCase()} ${day.substring(1)}');
+  // OOP in dart
+  // OOP- Object Oriented Programming uses objects and their inderaction to create design and program applications
+  //in OOP, an object can be anything.
+
+
+
+}
