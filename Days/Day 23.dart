3df82bc749f7d333441eb7444df239ece96cfcab
Glory Ejere <glorychristopher620@gmail.com>
2023-10-28 23:59:34 -1200

    .

--- Days/Day 23.dart
+++ Days/Day 23.dart
@@ -2,8 +2,9 @@ void main() {
   String day = 'day twenty-three';
   print('${day[0].toUpperCase()} ${day.substring(1)}');
   // OOP in dart
-  // OOP- Object Oriented Programming uses objects and their inderaction to create design and program applications
+  // OOP- Object Oriented Programming uses objects(object in OOP can be anything) and their inderaction to create design and program applications
   //in OOP, an object can be anything.
+
 
 
 
