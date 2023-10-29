76d5e60e49e2cb00b2e8339e42bccb6bd215c4e4
Glory Ejere <glorychristopher620@gmail.com>
2023-10-28 12:52:14 -1200

    Day 22

--- Days/Day 22.dart
+++ Days/Day 22.dart
@@ -13,7 +13,17 @@ void main() {
   print('My name is $name ');
 
   // Integer user input
-  print('Enter number:');
-  int? number = int.parse(stdin.readLineSync()!);
-  print('The number is $number');
+  print('Enter age:');
+  int? age = int.parse(stdin.readLineSync()!);
+  print('I am $age');
+
+  // Floating point user input
+  // float input can be used to get a numeric value from the user with the decimal point. 
+
+  print ("Correct value:");
+  // String? value = stdin.readLineSync();
+  int? value = int.parse(stdin.readLineSync()!);
+  double? decimalValue = double.parse(stdin.readLineSync()!);
+  double correctValue = decimalValue + value; // The value of the 'double' variant plus the value of the 'integer'
+  print('The correct value is $correctValue');
 }
