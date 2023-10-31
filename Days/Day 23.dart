0349b2023e0eaf7d9812c7e59e44e4f003545548
Glory Ejere <glorychristopher620@gmail.com>
2023-10-30 15:47:22 -1200

    Day 24

--- Days/Day 23.dart
+++ Days/Day 23.dart
@@ -11,9 +11,11 @@ void main() {
   // 3. It reduces the repitition of code.
 
   // Features of OOP:
-  // 1. Class
-  // 2. Object
-  // 3. Encapsulation
-
+  //1. Class
+  //2. Object
+  //3. Encapsulation
+  // 4. Inheritance
+  // 5. Polymorphism
+  // 6. Abstraction
 
 }
