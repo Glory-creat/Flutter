cb0e8b557e4d4ae6c882204edda49529f7eefaac
Glory Ejere <glorychristopher620@gmail.com>
2023-10-30 16:25:55 -1200

    Day 25

---
+++ Days/Day 25.dart
@@ -0,0 +1,24 @@
+    // class in dart
+    // You can declare a class in dart using the class keyword followed by class name and braces {}.
+    // Body of the class consists of properties and functions.
+    // Properties are used to store the data. It is also known as fields or attributes. Functions are used to perform the operations. 
+    // It is also known as methods.
+
+  import 'dart:ffi';
+
+class Userprofile {
+    // Considering these properties: Age, Name, Profession
+
+    String? name;
+    int? age;
+    String? profession;
+    Bool? isSingle;
+
+    void displayinfo() {
+      // The displayinfo is a method in class is used to print out value property
+      print('Name: $name');
+      print('Age: $age');
+      print('Profession: $profession');
+      print('Status: $isSingle');
+    }
+  }
