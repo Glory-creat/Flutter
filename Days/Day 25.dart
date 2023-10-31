d5aac56d2a928ecd5694353e5261cf6097540cea
Glory Ejere <glorychristopher620@gmail.com>
2023-10-30 16:53:40 -1200

    Day 25

--- Days/Day 25.dart
+++ Days/Day 25.dart
@@ -1,4 +1,5 @@
     // class in dart
+    //class is a blueprint for creating objects. It defines the properties and methods that an object will have
     // You can declare a class in dart using the class keyword followed by class name and braces {}.
     // Body of the class consists of properties and functions.
     // Properties are used to store the data. It is also known as fields or attributes. Functions are used to perform the operations. 
@@ -21,3 +22,19 @@ class Userprofile {
       print('Status: $isSingle');
     }
   }
+
+      // Task
+    // Create a class Book with three properties: name, author, and prize. Also, create a method called display, which prints out the values of the three properties.
+  class Book{
+    // String name = 'Glory Adaolisa';
+    String? name;
+    String? author;
+    int? prize;
+
+    void display() {
+      print('name');
+      print('author');
+      print('prize');
+    }
+  }
+
