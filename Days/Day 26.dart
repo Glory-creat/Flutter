1ad883ccd1ff1b479a8de10fc5513f1e74936fb3
Glory Ejere <glorychristopher620@gmail.com>
2023-10-31 17:21:10 -1200

    Day 26

--- Days/Day 26.dart
+++ Days/Day 26.dart
@@ -5,12 +5,24 @@ class Car{
   String? name;
   String? color;
   int? numberOfSeats;
-  Car(this.name, this.color, this.numberOfSeats);
+  int? price;
+  Car(this.name, this.color, this.numberOfSeats, this.price);
 
   void carProperties(){
     print('Name: $name');
     print('Car color: $color');
     print('Number Of Seats: $numberOfSeats');
+    print('Price: $price');
+  }
+
+  String highPrice(){
+    if(price! > 140000){
+      print('Price is high');
+    }else if(price! <= 140000 && price! < 13000) {
+      print('Price is low');
+    }else{
+      print('Price is normal');
+    }
   }
 }
 
@@ -18,14 +30,17 @@ void main() {
 
   // Create a Car object and initialize its properties
 
-  Car car = Car('Audi', 'Red', 6);
+  Car car = Car('Audi', 'Red', 6, 140000);
   // car.name = 'Audi';
   // car.color = 'Red';
   // car.numberOfSeats = 4;
 
   car.carProperties();
 
-  Car car2 = Car('Toyota', 'Grey', 6);
+  Car car2 = Car('Toyota', 'Grey', 6, 140000);
   car2.carProperties();
 
+  if(car.highPrice()){
+    print('Price is high ${car}');
+  }
 }
