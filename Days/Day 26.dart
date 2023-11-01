636db11c2adc981b3eebe5d7e59c8d543ccfb297
Glory Ejere <glorychristopher620@gmail.com>
2023-10-31 15:46:38 -1200

    Day 26: Objects in Dart

--- Days/Day 26.dart
+++ Days/Day 26.dart
@@ -7,7 +7,7 @@ class Car{
   int? numberOfSeats;
   Car(this.name, this.color, this.numberOfSeats);
 
-  void carFunction(){
+  void carProperties(){
     print('Name: $name');
     print('Car color: $color');
     print('Number Of Seats: $numberOfSeats');
@@ -15,13 +15,17 @@ class Car{
 }
 
 void main() {
-  
+
   // Create a Car object and initialize its properties
 
-  Car car = Car('Audi', 'Red', 4);
+  Car car = Car('Audi', 'Red', 6);
   // car.name = 'Audi';
   // car.color = 'Red';
   // car.numberOfSeats = 4;
 
-  car.carFunction();
+  car.carProperties();
+
+  Car car2 = Car('Toyota', 'Grey', 6);
+  car2.carProperties();
+
 }
