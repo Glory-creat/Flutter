64c7b89d40edfeb02c2c5e9c11669ec2b7010b33
Glory Ejere <glorychristopher620@gmail.com>
2023-11-04 21:23:45 -1200

    Day 28

--- Days/Day 28.dart
+++ Days/Day 28.dart
@@ -1,4 +1,7 @@
 // Day 28
 void main() {
+  // Default Constructor
+  // The default constructor is responsible for initializing instances of the class. If you don't 
+  //provide any constructors in your class, Dart provides an implicit default constructor.
   
 }
