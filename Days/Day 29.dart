e5c2189fd4bce70d14190978582d96ab64e8baaa
Glory Ejere <glorychristopher620@gmail.com>
2023-11-08 14:53:46 -1200

    Day 29

--- Days/Day 29.dart
+++ Days/Day 29.dart
@@ -1,12 +1,17 @@
 // Named constructor practice
 
 class Student{
-  String name;
-  int age;
-  double height;
-  String gender;
+  String? name;
+  int? age;
+  double? height;
+  String? gender;
 
-  Student(this.name, this.age, this.height, this.gender);
+  // Student(this.name, this.age, this.height, this.gender);
+
+  // Default constructors
+  Student(){
+    print('Named Constructor');
+  }
 
   // Named constructors
   Student.firstProfile(this.name, this.age, this.height, this.gender)  {
@@ -30,16 +35,22 @@ class Student{
 
 
   // void display() {
-  //   print('Name: $name');
-  //   print('Age: $age');
-  //   print('Height: $height');
-  //   print('Gender: $gender');
+  // //   print('Name: $name');
+  // //   print('Age: $age');
+  // //   print('Height: $height');
+  // //   print('Gender: $gender');
+  // print(Student);
   // }
 }
 
 
 void main() {
+  print(Student());
   Student.firstProfile('Glory', 19, 6.2, 'Female'); //Named constructor 1
   Student.secondProfile('Blessing', 23, 6.2, 'Female'); //Named constructor 2
   Student.thirdProfile('Adeola', 19, 6.5, 'Female'); //Named constructor 3
 }
+
+
+
+// Constant constructor in Dart
