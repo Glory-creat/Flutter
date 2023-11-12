d03f374b3c276622708b13d4f30e1a857a5c3f1b
Glory Ejere <glorychristopher620@gmail.com>
2023-11-12 05:10:04 -1200

    Day 29

--- Days/Day 29.dart
+++ Days/Day 29.dart
@@ -1,55 +1,55 @@
 // Named constructor practice
 
-class Student{
-  String? name;
-  int? age;
-  double? height;
-  String? gender;
+// class Student{
+//   String? name;
+//   int? age;
+//   double? height;
+//   String? gender;
 
-  // Student(this.name, this.age, this.height, this.gender);
+//   // Student(this.name, this.age, this.height, this.gender);
 
-  // Default constructors
-  Student(){
-    print('Named Constructor');
-  }
+//   // Default constructors
+//   Student(){
+//     print('Named Constructor');
+//   }
 
-  // Named constructors
-  Student.firstProfile(this.name, this.age, this.height, this.gender)  {
-    print('Name: $name');
-    print('Age: $age');
-    print('Height: $height');
-    print('Gender: $gender');
-  }
-   Student.secondProfile(this.name, this.age, this.height, this.gender)  {
-    print('Name: $name');
-    print('Age: $age');
-    print('Height: $height');
-    print('Gender: $gender');
-  }
-  Student.thirdProfile(this.name, this.age, this.height, this.gender)  {
-    print('Name: $name');
-    print('Age: $age');
-    print('Height: $height');
-    print('Gender: $gender');
-  }
-
-
-  // void display() {
-  // //   print('Name: $name');
-  // //   print('Age: $age');
-  // //   print('Height: $height');
-  // //   print('Gender: $gender');
-  // print(Student);
-  // }
-}
+//   // Named constructors
+//   Student.firstProfile(this.name, this.age, this.height, this.gender)  {
+//     print('Name: $name');
+//     print('Age: $age');
+//     print('Height: $height');
+//     print('Gender: $gender');
+//   }
+//    Student.secondProfile(this.name, this.age, this.height, this.gender)  {
+//     print('Name: $name');
+//     print('Age: $age');
+//     print('Height: $height');
+//     print('Gender: $gender');
+//   }
+//   Student.thirdProfile(this.name, this.age, this.height, this.gender)  {
+//     print('Name: $name');
+//     print('Age: $age');
+//     print('Height: $height');
+//     print('Gender: $gender');
+//   }
 
 
-void main() {
-  print(Student());
-  Student.firstProfile('Glory', 19, 6.2, 'Female'); //Named constructor 1
-  Student.secondProfile('Blessing', 23, 6.2, 'Female'); //Named constructor 2
-  Student.thirdProfile('Adeola', 19, 6.5, 'Female'); //Named constructor 3
-}
+//   // void display() {
+//   // //   print('Name: $name');
+//   // //   print('Age: $age');
+//   // //   print('Height: $height');
+//   // //   print('Gender: $gender');
+//   // print(Student);
+//   // }
+// }
+
+
+// void main() {
+//   print(Student());
+//   Student.firstProfile('Glory', 19, 6.2, 'Female'); //Named constructor 1
+//   Student.secondProfile('Blessing', 23, 6.2, 'Female'); //Named constructor 2
+//   Student.thirdProfile('Adeola', 19, 6.5, 'Female'); //Named constructor 3
+// }
 
 
 
@@ -57,3 +57,19 @@ void main() {
 // Constant constructor is a constructor that creates a constant object.
 // All properties of the class must be final.
 
+// Example from site
+class Point{
+  // Properties
+  final int x;
+  final int y;
+
+  const Point(this.x, this.y);
+}
+void main() {
+  Point point = const Point(30, 45);
+  print('The result is ${point.hashCode}');
+
+  //without const
+  Point point1 = Point(4, 2);
+  print('The result is ${point1.y}');
+}
