0528c02b4217d5bc0805a4907e7436d4b5bc9944
Glory Ejere <glorychristopher620@gmail.com>
2023-11-19 22:20:30 -1200

    Day 30

--- Days/Day 30.dart
+++ Days/Day 30.dart
@@ -3,3 +3,5 @@
 // Encapsulation is a mechanism for hiding important and sensitive data from users.
 
 // Examples
+// Encapsulation means hiding data within a library, preventing it from outside factors. 
+//It helps you control your program and prevent it from becoming too complicated.
