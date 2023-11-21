6b03ead5f3b4abcd8ab6141504efe20ce1ddbcff
Glory Ejere <glorychristopher620@gmail.com>
2023-11-20 23:46:32 -1200

    Day 30

--- Days/Day 30.dart
+++ Days/Day 30.dart
@@ -1,7 +1,7 @@
 // Day 30
 //Encapsulation in dart
 // Encapsulation is a mechanism for hiding important and sensitive data from users.
-
-// Examples
 // Encapsulation means hiding data within a library, preventing it from outside factors. 
 //It helps you control your program and prevent it from becoming too complicated.
+
+// Example 1
