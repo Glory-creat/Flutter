8ac14b2dbbcbc5d03f89687ffd60cf920bc4c0bd
Glory Ejere <glorychristopher620@gmail.com>
2023-12-04 23:56:48 -1200

    Setters

--- Days/Day 31.dart
+++ Days/Day 31.dart
@@ -6,24 +6,26 @@
 // properties _name and _prize. There are two getters name and prize to access the value of 
 // the properties. If you provide a blank name, then it will return No Name.
 
-class Notebook{
-  String? _name;
-  double? _prize;
+// class Notebook{
+//   String? _name;
+//   double? _prize;
 
-  // consructor
-  Notebook(this._name, this._prize);
+//   // consructor
+//   Notebook(this._name, this._prize);
   
-  String get name => this._name!;
-  double get prize => this._prize!;
-}
+//   String get name => this._name!;
+//   double get prize => this._prize!;
+// }
 
-void main() {
-  Notebook Note = Notebook('Glory Book', 50);
-  print('Notebook name: ${Note.name}');
-  print('Notebook prize: ${Note.prize}');
+// void main() {
+//   Notebook Note = Notebook('Glory Book', 50);
+//   print('Notebook name: ${Note.name}');
+//   print('Notebook prize: ${Note.prize}');
 
-  print('Notebook 2');
-  Notebook Note2 = Notebook('Sayo Book', 15);
-  print('Notebook name: ${Note2.name}');
-  print('Notebook prize: ${Note2.prize}');
-}
+//   print('Notebook 2');
+//   Notebook Note2 = Notebook('Sayo Book', 15);
+//   print('Notebook name: ${Note2.name}');
+//   print('Notebook prize: ${Note2.prize}');
+// }
+
+// Setters in Dart
